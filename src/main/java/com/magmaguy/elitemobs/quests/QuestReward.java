10d5f16cce98a4a128a3c34687c78d43a7a1ab98
Mfweb <h@i2s.io>
2020-11-12 14:22:06 +0800

    任务收益也会受到刷怪收益上限影响

--- src/main/java/com/magmaguy/elitemobs/quests/QuestReward.java
+++ src/main/java/com/magmaguy/elitemobs/quests/QuestReward.java
@@ -4,6 +4,8 @@ import com.magmaguy.elitemobs.adventurersguild.GuildRank;
 import com.magmaguy.elitemobs.config.EconomySettingsConfig;
 import com.magmaguy.elitemobs.config.menus.premade.QuestMenuConfig;
 import com.magmaguy.elitemobs.economy.EconomyHandler;
+import com.magmaguy.elitemobs.playerdata.PlayerData;
+
 import org.bukkit.entity.Player;
 
 import java.io.Serializable;
@@ -46,9 +48,15 @@ public class QuestReward implements Serializable {
 
     public void doReward(Player player) {
         if (rewardType.equals(RewardType.MONETARY)) {
-            EconomyHandler.addCurrency(player.getUniqueId(), this.questReward * GuildRank.currencyBonusMultiplier(GuildRank.getGuildPrestigeRank(player)));
+            double c = this.questReward * GuildRank.currencyBonusMultiplier(GuildRank.getGuildPrestigeRank(player));
+            PlayerData.addCurrencyDayCount(player.getUniqueId(), c);
+            double r = PlayerData.getCurrencyDayRatio(player.getUniqueId());
+            EconomyHandler.addCurrency(player.getUniqueId(), c * r);
+            if(r < 1.0) {
+                player.sendMessage("今日猎杀博格收益已达到上限，当前收益为:§c" + String.format("%.2f", r * 100 - 0.005) + "%");
+            }
             player.sendMessage(QuestMenuConfig.rewardMessage
-                    .replace("$reward", this.questReward * GuildRank.currencyBonusMultiplier(GuildRank.getGuildPrestigeRank(player)) + "")
+                    .replace("$reward", String.format("%.2f", c * r))
                     .replace("$currencyName", EconomySettingsConfig.currencyName));
         }
     }
